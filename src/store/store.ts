0c7e13291507599a3bd89f98691d323b6556a798
Md Amman <md.krazio@gmail.com>
2026-03-24 20:31:48 +0530

    migrate zustand global store to typescript

---
+++ src/store/store.ts
@@ -0,0 +1,52 @@
+import { create } from 'zustand';
+import { IChat, IMessage } from '@/types/chat';
+
+interface IAppState {
+    input: string;
+    setInput: (input: string) => void;
+
+    isNewChat: boolean;
+    setIsNewChat: (isNewChat: boolean) => void;
+
+    chats: IChat[];
+    removeChat: (chatId: string) => void;
+    setChats: (chats: IChat[]) => void;
+
+    messages: IMessage[];
+    updateMessages: (newMessage: IMessage) => void;
+    setMessages: (messages: IMessage[]) => void;
+
+    loading: boolean;
+    setLoading: (loading: boolean) => void;
+
+    sidebarIsOpen: boolean;
+    setSidebarIsOpen: () => void;
+}
+
+const useAppStore = create<IAppState>((set) => ({
+    input: '',
+    setInput: (input) => set({ input }),
+
+    isNewChat: false,
+    setIsNewChat: (isNewChat) => set({ isNewChat }),
+
+    chats: [],
+    removeChat: (chatId) => {
+        set((state) => ({
+            chats: state.chats.filter((chat) => chat.referenceId !== chatId),
+        }));
+    },
+    setChats: (chats) => set({ chats }),
+
+    messages: [],
+    updateMessages: (newMessage) => set((state) => ({ messages: [...state.messages, newMessage] })),
+    setMessages: (messages) => set({ messages }),
+
+    loading: false,
+    setLoading: (loading) => set({ loading }),
+
+    sidebarIsOpen: false,
+    setSidebarIsOpen: () => set((state) => ({ sidebarIsOpen: !state.sidebarIsOpen })),
+}));
+
+export default useAppStore;
