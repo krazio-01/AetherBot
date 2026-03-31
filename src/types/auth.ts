a4c8b10e884fe542dabef216cfdeb73b197d4a13
Md Amman <md.krazio@gmail.com>
2026-03-31 23:16:53 +0530

    create type for auth pages

---
+++ src/types/auth.ts
@@ -0,0 +1,20 @@
+export interface ISignupRequest {
+    name: string;
+    email: string;
+    password: string;
+    avatar: string;
+}
+
+export interface ILoginRequest {
+    identifier: string;
+    password: string;
+}
+
+export interface IPasswordResetRequest {
+    email: string;
+}
+
+export interface IPasswordChangeRequest {
+    token: string;
+    newPassword: string;
+}
