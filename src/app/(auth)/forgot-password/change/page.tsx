5ebc70a3e9be87a4628d4058b7523887a44af0b0
Md Amman <md.krazio@gmail.com>
2026-03-31 23:21:09 +0530

    refactor forgot-password change comp to use custom useRequest hook

--- src/app/(auth)/forgot-password/change/page.tsx
+++ src/app/(auth)/forgot-password/change/page.tsx
@@ -1,32 +1,23 @@
 'use client';
-import { useRef, useState, Suspense } from 'react';
+import { useRef, Suspense } from 'react';
 import { useSearchParams } from 'next/navigation';
 import AuthForm from '@/components/forms/AuthForm';
-import axios, { AxiosError } from 'axios';
+import { useRequest } from '@/hooks/useRequest';
 import { MdLockReset } from 'react-icons/md';
 import { FaLock } from 'react-icons/fa';
 import { IAuthField } from '@/types';
+import { IPasswordChangeRequest } from '@/types/auth';
 import '../../auth.css';
 
 const PageInner = () => {
-    const [loading, setLoading] = useState(false);
+    const { postRequest, isPending } = useRequest();
 
     const newPasswordRef = useRef<HTMLInputElement>(null);
     const confirmNewPasswordRef = useRef<HTMLInputElement>(null);
 
     const formFields: IAuthField[] = [
-        {
-            name: 'password',
-            label: 'New Password',
-            type: 'password',
-            icon: <FaLock />,
-        },
-        {
-            name: 'confirm-password',
-            label: 'Password',
-            type: 'password',
-            icon: <FaLock />,
-        },
+        { name: 'password', label: 'New Password', type: 'password', icon: <FaLock /> },
+        { name: 'confirm-password', label: 'Confirm Password', type: 'password', icon: <FaLock /> },
     ];
     const refs = [newPasswordRef, confirmNewPasswordRef];
 
@@ -39,16 +30,12 @@ const PageInner = () => {
 
         if (newPassword !== confirmPassword) throw 'Passwords do not match';
 
-        try {
-            setLoading(true);
-            const { data } = await axios.post('/api/auth/forgot-password/change', { token, newPassword });
-            return data.message;
-        } catch (error) {
-            if (error instanceof AxiosError) throw error.response?.data?.message || 'Failed to reset password.';
-            throw 'An unexpected error occurred.';
-        } finally {
-            setLoading(false);
-        }
+        const res = await postRequest<void, IPasswordChangeRequest>('/auth/forgot-password/change', {
+            token,
+            newPassword,
+        });
+
+        return res.message;
     };
 
     return (
@@ -57,12 +44,7 @@ const PageInner = () => {
                 <div className="auth-form-header">
                     <MdLockReset />
                     <h2>Reset your password</h2>
-                    <p
-                        style={{
-                            fontSize: 'var(--fz-sm)',
-                            textAlign: 'center',
-                        }}
-                    >
+                    <p style={{ fontSize: 'var(--fz-sm)', textAlign: 'center' }}>
                         Enter a new password below to reset your password
                     </p>
                 </div>
@@ -70,7 +52,7 @@ const PageInner = () => {
                 <AuthForm
                     formFields={formFields}
                     refs={refs}
-                    loading={loading}
+                    loading={isPending}
                     onSubmit={handleResetPassword}
                     loadingText="Updating password..."
                     redirectUrl="/login"
