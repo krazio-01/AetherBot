4e62ed15c7f4e5d7a57b8f64fb8c0d986998ee03
Md Amman <md.krazio@gmail.com>
2026-04-06 17:26:35 +0530

    hide sidebar toggle outside of chat page

--- src/components/Ui/Sidebar-Toggle/ToggleButton.tsx
+++ src/components/Ui/Sidebar-Toggle/ToggleButton.tsx
@@ -1,11 +1,16 @@
 'use client';
-import './toggleButton.css';
 import { TbLayoutSidebarLeftExpand, TbLayoutSidebarLeftCollapse } from 'react-icons/tb';
 import useAppStore from '@/store/store';
+import { usePathname } from 'next/navigation';
+import './toggleButton.css';
 
 const ToggleButton = () => {
     const setSidebarIsOpen = useAppStore((state) => state.setSidebarIsOpen);
     const sidebarIsOpen = useAppStore((state) => state.sidebarIsOpen);
+
+    const pathname = usePathname();
+
+    if (pathname !== '/chat') return null;
 
     return (
         <div>
